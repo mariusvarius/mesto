8b034108ad9da03ad24070f54589075c056e0c1d
Maria <mari.dm.var@gmail.com>
2020-04-15 23:12:09 +0300

    функция

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-document.getElementById("edit-button").addEventListener("click", function() {
-    document.querySelector(".popup").style.display = "flex";
-});
+const editButton = document.querySelector('.edit-button');
+const popupActive = document.querySelector('.popup');
 
-document.querySelector(".close-button").addEventListener("click", function() {
-    document.querySelector(".popup").style.display = "none";
-});
+function popupActivate() {
+    popupActive.classList.add.('popup__is_active');
+}
+editButton.addEventListener('click', popupActivate);
+
+let title = document.querySelector('.profile__title');
+let subtitle = document.querySelector('.profile__subtitle');
+let button = document.querySelector('.button');
+
+let newTitle = document.querySelector('.popup-name');
+let newSubtitle = document.querySelector('.popup-job');
+
+function renameForm() {
+    title = newTitle.nodeValue;
+    title = newSubtitle.nodeValue;
+}
+
+button.addEventListener('click', renameForm);
